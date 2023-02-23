1d34f607a77949377b2f0df12af4ca9fba9949ea
Hamish Murphy <hamishmurphy@gmail.com>
2023-02-23 16:58:22 +1100

    Request form

--- BlazorServerExample/Pages/RequestViewModel.cs
+++ BlazorServerExample/Pages/RequestViewModel.cs
@@ -2,5 +2,10 @@
 
 public class RequestViewModel
 {
+    public string Endpoint { get; set; } = "http://blah.com";
+    public string ChannelName { get; set; } = "/fred";
+    public string Topic { get; set; } = "Yo!";
+
+    public string Id { get; set; } = "";
     public string Message { get; set; } = "";
 }
