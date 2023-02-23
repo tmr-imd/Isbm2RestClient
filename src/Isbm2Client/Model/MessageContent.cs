c3a1c74f87c4f57ac4d7f307dadb99ff74f18409
Hamish Murphy <hamishmurphy@gmail.com>
2023-02-23 14:19:19 +1100

    :fire:

--- src/Isbm2Client/Model/MessageContent.cs
+++ src/Isbm2Client/Model/MessageContent.cs
@@ -22,16 +22,6 @@ public abstract record class MessageContent( string Id )
 
     public T Deserialise<T>() where T : class, new()
     {
-        if ( instance is string json )
-        {
-            var result = JsonSerializer.Deserialize<T>( json );
-
-            if (result is null)
-                throw new InvalidCastException($"Could not convert instance into: {typeof(T).Name}" );
-
-            return result;
-        }
-
         if ( instance is Dictionary<string, object> dictionary )
         {
             return ObjectExtensions.ToObject<T>( dictionary );
@@ -44,13 +34,11 @@ public abstract record class MessageContent( string Id )
     {
         if ( instance is not string && instance is not Dictionary<string, object> )
         {
-            instance = ObjectExtensions.AsDictionary(instance);
-
-            //throw new ArgumentException("Invalid instance found. Must be the following types: string or Dictionary<string, object>");
+            throw new ArgumentException("Invalid instance found. Must be the following types: string or Dictionary<string, object>");
         }
 
         this.instance = instance;
     }
 }
 public record class MessageString( string Id, string Content ) : MessageContent( Id, Content );
-public record class MessageDictionary(string Id, Dictionary<string, object> Content) : MessageContent(Id, Content);
+public record class MessageJsonDocument(string Id, Dictionary<string, object> Content) : MessageContent(Id, Content);
