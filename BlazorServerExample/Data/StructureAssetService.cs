79522c8b326360c59553a691c3ff546d6fd312ba
Hamish Murphy <hamishmurphy@gmail.com>
2023-02-24 10:49:07 +1100

    Filtering bugs

--- BlazorServerExample/Data/StructureAssetService.cs
+++ BlazorServerExample/Data/StructureAssetService.cs
@@ -15,22 +15,22 @@ namespace BlazorServerExample.Data
             var records = csv.GetRecords<StructureAsset>();
 
             if ( filterCode != "" )
-                records = records.Where( x => x.Code.Contains(filterCode) );
+                records = records.Where( x => x.Code.ToLower().Contains(filterCode.ToLower()) );
 
             if (filterType != "")
-                records = records.Where(x => x.Code.Contains(filterType));
+                records = records.Where(x => x.Type.ToLower().Contains(filterType.ToLower()));
 
             if (filterLocation != "")
-                records = records.Where(x => x.Code.Contains(filterLocation));
+                records = records.Where(x => x.Location.ToLower().Contains(filterLocation.ToLower()));
 
             if (filterOwner != "")
-                records = records.Where(x => x.Code.Contains(filterOwner));
+                records = records.Where(x => x.Owner.ToLower().Contains(filterOwner.ToLower()));
 
             if (filterCondition != "")
-                records = records.Where(x => x.Code.Contains(filterCondition));
+                records = records.Where(x => x.Condition.ToLower().Contains(filterCondition.ToLower()));
 
             if (filterInspector != "")
-                records = records.Where(x => x.Code.Contains(filterInspector));
+                records = records.Where(x => x.Inspector.ToLower().Contains(filterInspector.ToLower()));
 
             return records.ToArray();
         }
