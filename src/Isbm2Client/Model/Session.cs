645fda17cfdbeb17c70a4a568d94e7f1f75f6ae8
Hamish Murphy <hamishmurphy@gmail.com>
2023-02-27 09:10:41 +1100

    Listener for provider
    Co-authored-by: Matt Selway <30459857+mattys101@users.noreply.github.com>

--- src/Isbm2Client/Model/Session.cs
+++ src/Isbm2Client/Model/Session.cs
@@ -3,5 +3,5 @@
 public abstract record class Session(string Id, string? ListenerUri, string[] Topics, string[] FilterExressions);
 public record class PublicationProviderSession(string Id) : Session(Id, null, Array.Empty<string>(), Array.Empty<string>());
 public record class PublicationConsumerSession(string Id, string? ListenerUri, string[] Topics, string[] FilterExressions) : Session(Id, ListenerUri, Topics, FilterExressions);
-public record class RequestProviderSession(string Id, string[] Topics, string[] FilterExressions) : Session(Id, null, Topics, FilterExressions);
+public record class RequestProviderSession(string Id, string? ListenerUri, string[] Topics, string[] FilterExressions) : Session(Id, ListenerUri, Topics, FilterExressions);
 public record class RequestConsumerSession(string Id, string? ListenerUri) : Session(Id, ListenerUri, Array.Empty<string>(), Array.Empty<string>());
