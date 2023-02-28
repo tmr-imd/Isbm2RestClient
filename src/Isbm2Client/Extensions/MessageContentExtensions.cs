fc7ae3174171622c092991ab8250f7177f0b9282
Hamish Murphy <hamishmurphy@gmail.com>
2023-02-28 18:27:58 +1100

    Suppress warning

--- src/Isbm2Client/Extensions/MessageContentExtensions.cs
+++ src/Isbm2Client/Extensions/MessageContentExtensions.cs
@@ -7,11 +7,15 @@ public static class MessageContentExtensions
 {
     public static RestModel.MessageContent ToRestMessageContent( this MessageContent messageContent )
     {
+// I'm not sure why the CS8604 is necessary. The Isbm2RestClient project doesn't have `nullable=enable`. So
+// RestModel.MessageContent.ContentEncoding should be equivalent to a `string?`
+#pragma warning disable CS8604 // Possible null reference argument.
         return new RestModel.MessageContent
         (
             messageContent.MediaType, 
             messageContent.ContentEncoding, 
             new RestModel.MessageContentContent( messageContent.Content )
         );
+#pragma warning restore CS8604 // Possible null reference argument.
     }
 }
