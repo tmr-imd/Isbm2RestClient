d7a4d19ce330cfcfbca95bebd3aebc25198becad
Hamish Murphy <hamishmurphy@gmail.com>
2023-03-16 17:50:39 +1100

    Simplify Program startup

---
+++ BlazorServerExample/Extensions/IsbmClientExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Isbm2Client.Interface;
+using Isbm2Client.Model;
+using Isbm2Client.Service;
+using Isbm2RestClient.Api;
+
+namespace BlazorServerExample.Extensions;
+
+public static class IsbmClientExtensions
+{
+    public static void AddIsbmRestClient( this IServiceCollection services, ClientConfig config )
+    {
+        services.AddScoped<IConsumerRequestServiceApi>(x => new ConsumerRequestServiceApi(config.EndPoint));
+        services.AddScoped<IProviderRequestServiceApi>(x => new ProviderRequestServiceApi(config.EndPoint));
+
+        services.AddScoped<IChannelManagement, RestChannelManagement>();
+        services.AddScoped<IProviderRequest, RestProviderRequest>();
+        services.AddScoped<IConsumerRequest, RestConsumerRequest>();
+        services.AddScoped<IProviderPublication, RestProviderPublication>();
+        services.AddScoped<IConsumerPublication, RestConsumerPublication>();
+    }
+}
--- BlazorServerExample/Program.cs
+++ BlazorServerExample/Program.cs
@@ -1,10 +1,7 @@
 using BlazorServerExample.Data;
+using BlazorServerExample.Extensions;
 using BlazorServerExample.Pages;
-using Isbm2Client.Interface;
 using Isbm2Client.Model;
-using Isbm2Client.Service;
-using Isbm2RestClient.Api;
-using RestSharp;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,28 +10,18 @@ builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<StructureAssetService>();
 
-// Example of how you would use Microsoft DI with IsbmClient
+// This will go away soon...
 builder.Services.Configure<ClientConfig>(builder.Configuration.GetSection("Isbm"));
 
-var endPoint = builder.Configuration.GetSection("Isbm").GetValue<string>("EndPoint");
-
-builder.Services.AddScoped<IConsumerRequestServiceApi>( x => new ConsumerRequestServiceApi(endPoint) );
-builder.Services.AddScoped<IProviderRequestServiceApi>( x => new ProviderRequestServiceApi(endPoint) );
-
-builder.Services.AddScoped<IChannelManagement, RestChannelManagement>();
-builder.Services.AddScoped<IProviderRequest, RestProviderRequest>();
-builder.Services.AddScoped<IConsumerRequest, RestConsumerRequest>();
-builder.Services.AddScoped<IProviderPublication, RestProviderPublication>();
-builder.Services.AddScoped<IConsumerPublication, RestConsumerPublication>();
+var clientConfig = new ClientConfig();
+builder.Configuration.GetSection( "Isbm" ).Bind( clientConfig );
+builder.Services.AddIsbmRestClient( clientConfig );
 
 builder.Services.AddScoped<StructureAssetService>();
 
 builder.Services.AddScoped<RequestViewModel>();
 builder.Services.AddScoped<PublicationViewModel>();
 
-// Once the client grows, we could create a single helper method like this:
-//builder.Services.Configure<ClientConfig>(builder.Configuration.GetSection("Isbm"));
-//builder.Service.AddIsbmClient();
 
 var app = builder.Build();
 
