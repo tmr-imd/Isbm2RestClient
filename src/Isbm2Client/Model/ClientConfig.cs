b8300b0d42d580192cd4969b7aaa2b4c34b8cac9
Matt Selway <30459857+mattys101@users.noreply.github.com>
2023-08-31 23:17:34 +0930

    Added ListenerUrlBase to ClientConfig

--- src/Isbm2Client/Model/ClientConfig.cs
+++ src/Isbm2Client/Model/ClientConfig.cs
@@ -5,4 +5,5 @@ namespace Isbm2Client.Model;
 public record ClientConfig
 {
     public string EndPoint { get; init; } = "http://localhost/rest";
+    public string ListenerUrlBase { get; init; } = "";
 }
